7c1959a6c59dacacef5f829b8b2a7c2aad88ca29
molmoty <molmoty@gmail.com>
2023-03-07 15:20:19 +0900

    토큰 세션개념 추가

--- app/middlewares/session.py
+++ app/middlewares/session.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import pynecone
 
 from app import Global, Session
+from app.models.auth import AuthUserAccessLog, AuthUser
 
 
 class SessionMiddleware(pynecone.Middleware):
@@ -9,6 +12,27 @@ class SessionMiddleware(pynecone.Middleware):
             state.session = Session(
                 token=event.router_data["token"], ip=event.router_data["ip"]
             )
+
+        with pynecone.session() as s:
+            if state.session.user_id is None:
+                if (
+                    token_user_log := s.query(AuthUserAccessLog)
+                    .filter(AuthUserAccessLog.token.__eq__(state.session.token))
+                    .one_or_none()
+                ):
+                    state.session.user_id = token_user_log.user_id
+
+            if state.session.user_id is not None:
+                s.add(
+                    AuthUserAccessLog(
+                        user_id=state.session.user_id,
+                        token=state.session.token,
+                        ip=state.session.ip,
+                        reg_datetime=datetime.now(),
+                    )
+                )
+
+            s.commit()
 
     def postprocess(self, app: pynecone.App, state: Global.State, event, delta):
         pass
